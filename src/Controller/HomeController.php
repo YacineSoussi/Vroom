cbeb897e773ff636956e4ae21f6fee6d3c6a2cbb
Yacine <Yacine1.soussi@gmail.com>
2019-09-18 18:45:52 +0200

    installation et page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends Controller{
+
+    /**
+     * @Route("/hello/{prenom}/age/{age}", name="hello")
+     * @Route("/hello", name="hello_base")
+     * @Route("/hello/{prenom}", name="hello_prenom")
+     * 
+     * Montre la page qui dit bonjour
+     * @return void
+     */
+    public function hello($prenom = "anonyme", $age = 0) {
+        return $this->render(
+            'hello.html.twig',
+            [
+                'prenom' => $prenom,
+                'age' => $age
+            ]
+            );
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(){
+        $prenom = ["Lior" => 31, "Joseph" => 12, "Anne" => 55];
+        return $this->render(
+            'home.html.twig'
+        );
+    }
+}
+?>
